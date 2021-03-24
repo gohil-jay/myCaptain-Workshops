3e1b159567908e770156cf49bc68df8a08053061
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-03-24 11:47:10 +0530

    Added titles and labels to the visual

--- Python-Data-Analytics/task4.py
+++ Python-Data-Analytics/task4.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Creating the minor dataset
+# Creating the dataset
 data = {'Math-I':80, 'Chemistry':87, 'Comm. Skills':88, 'EME':74, 'EEE':95, 'PEHV':74, 'Workshop':88, 'EME':74, 'Sports-I':81}
 course = list(data.keys())
 marks = list(data.values())
@@ -11,4 +11,8 @@ fig = plt.figure(figsize = (10, 5))
 
 # creating the bar plot
 plt.bar(course, marks, color ='green', width = 0.4)
+
+plt.xlabel("Course")
+plt.ylabel("Marks")
+plt.title("Course vs. Marks bar plot via Matplotlib")
 plt.show()
