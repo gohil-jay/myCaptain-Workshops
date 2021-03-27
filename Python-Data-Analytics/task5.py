61bb9e2ceb9cbda2c60ade3232a43ff4d2b62b7e
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-03-27 09:12:46 +0530

    Added further data and plot titles

--- Python-Data-Analytics/task5.py
+++ Python-Data-Analytics/task5.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Creating the dataset
-data = {'7AM':4, '8AM':4.5, '9AM':4.5, '10AM':5, '11AM':5, '12PM':4.5, '1PM':4, '2PM':4.5, '3PM':3.5, '4PM':4, '5PM':4.5}
+data = {'7AM':4, '8AM':4.5, '9AM':4.5, '10AM':5, '11AM':5, '12PM':4.5, '1PM':4, '2PM':4.5, '3PM':3.5, '4PM':4, '5PM':4.5, '6PM':4.5, '7PM':4, '8PM':3.5, '9PM':3, '10PM':2}
 time = list(data.keys())
 prod = list(data.values())
 
@@ -11,4 +11,8 @@ fig = plt.figure(figsize = (10, 5))
 
 # creating the bar plot
 plt.bar(time, prod, color ='green', width = 0.5)
+
+plt.xlabel("- Time of day -")
+plt.ylabel("Productivity")
+plt.title("Task - Bivariate Analysis using Bar Plots")
 plt.show()
