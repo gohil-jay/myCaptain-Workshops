a36b3319eb782f54fb6b2bb980e9fbfe5bca216e
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-03-29 11:43:24 +0530

    Added input and input validation

--- Python/school-management-system.py
+++ Python/school-management-system.py
@@ -8,3 +8,13 @@ def write_csv(list):
             write.writerow(["Name", "Age", "Contact No.", "Email ID"])
 
         write.writerow(list)
+
+condition = True
+st_num = 1
+
+while (condition == 1):
+    st_info = input("Enter student information for student #{} in following format - (Name Age Contact No. Email ID): ".format(st_num))
+    st_info_list = st_info.split(' ')
+    
+    print("\nThe entered information -->\nName: {}\nAge: {}\nContact No.: {}\nEmal ID: {}".format(st_info_list[0], st_info_list[1], st_info_list[2], st_info_list[3]))
+    print("")
