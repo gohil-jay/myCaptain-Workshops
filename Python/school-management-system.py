bd731cc6e9f59f08a05be111015d220edeb651bb
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-04-02 09:46:23 +0530

    Added continuous loop for input & finished project

--- Python/school-management-system.py
+++ Python/school-management-system.py
@@ -9,26 +9,32 @@ def write_csv(list):
 
         write.writerow(list)
 
-condition = True
-st_num = 1
+if __name__ == "__main__":
 
-while (condition == 1):
-    st_info = input("Enter student information for student #{} in following format - (Name Age Contact No. Email ID): ".format(st_num))
-    st_info_list = st_info.split(' ')
-    
-    print("\nThe entered information -->\nName: {}\nAge: {}\nContact No.: {}\nEmal ID: {}".format(st_info_list[0], st_info_list[1], st_info_list[2], st_info_list[3]))
+    condition = True
+    st_num = 1
+
     print("")
+    print("------------------------------------- WELCOME TO SCHOOL MANAGEMENT SYSTEM -------------------------------------")
+    print("")
+    while (condition == 1):
+        st_info = input("Enter student information for student #{} in following format - (Name Age Contact No. Email ID): ".format(st_num))
+        st_info_list = st_info.split(' ')
 
-    choice = input("Is the entered information correct (yes/no) : ")
-    if (choice == "yes"):
-        write_csv(st_info_list)
-        condition_check = input("Want to continue adding another student's information (yes/no) : ")
+        print("\nThe entered information -->\nName: {}\nAge: {}\nContact No.: {}\nEmal ID: {}".format(st_info_list[0], st_info_list[1], st_info_list[2], st_info_list[3]))
         print("")
+        choice = input("Is the entered information correct (yes/no) : ")
+
+        if (choice == "yes"):
+            write_csv(st_info_list)
+
+            condition_check = input("Want to continue adding another student's information (yes/no) : ")
+            print("")
             if (condition_check == "yes"):
                 condition = True
                 st_num += 1
             elif (condition_check == "no"):
                 condition = False
-    elif (choice == "no"):
-        print("\nPlease continue adding another student's information.")
-        print("")
+        elif (choice == "no"):
+            print("\nPlease continue adding another student's information.")
+            print("")
