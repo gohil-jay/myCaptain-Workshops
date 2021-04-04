32e82ca54c06be24fd26fa21a7f2d83b21d869e6
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-04-04 09:31:24 +0530

    Added web parsing code

--- Python/web-scraping.py
+++ Python/web-scraping.py
@@ -3,5 +3,11 @@ from bs4 import BeautifulSoup
 import pandas
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--page_num_max", helps="Enter the number of pages to parse", type = int)
+parser.add_argument("--dbname", helps="Enter the number of pages to parse", type = int)
+args = parser.parse_args()
+
+max_page = args.page_num_max
 url = "URL goes here"
 scrapped_list = []
