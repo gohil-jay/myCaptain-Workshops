0be9dce81053941eee550c9d1fe73e9fdf09de8f
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-04-05 08:19:09 +0530

    Connected 'connect' function from diff. py file

--- Python/web-scraping.py
+++ Python/web-scraping.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas
+# import pandas (only for CSVs)
 import argparse
+import web-sql
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--page_num_max", helps="Enter the number of pages to parse", type = int)
@@ -11,3 +12,4 @@ args = parser.parse_args()
 max_page = args.page_num_max
 url = "URL goes here"
 scrapped_list = []
+web-sql.connect(args.dbname)
