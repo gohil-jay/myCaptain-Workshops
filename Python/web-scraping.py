2e682e6cc32943bff723bfa21085816fe1133ee5
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-04-07 08:35:28 +0530

    Added data manipulation code of scrapped data

--- Python/web-scraping.py
+++ Python/web-scraping.py
@@ -10,6 +10,16 @@ parser.add_argument("--dbname", helps="Enter the number of pages to parse", type
 args = parser.parse_args()
 
 max_page = args.page_num_max
-url = "URL goes here"
+url = "https://www.oyorooms.com/hotels-in-bangalore/?page="
 scrapped_list = []
 web-sql.connect(args.dbname)
+
+for page_num in range(1, max_page+1):
+  final_url = url + str(page_num)
+  print("Get from URL", + final_url)
+  req = requests.get(final_url)
+  content = req.content
+  
+  soup = BeautifulSoup(content, "html.parser")
+  hotels = soup.find_all("div", {"class": "hotelCardListing"})
+  print(hotels)
